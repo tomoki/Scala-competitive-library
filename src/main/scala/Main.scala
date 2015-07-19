1593080adb28a99aaf1a22ddf06b55e61f3dee86
Tomoki Imai <tomo832@gmail.com>
2015-07-19 23:35:17 +0900

    Add faster io. (testing)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,30 +19,104 @@ package net.pushl {
   package datastructure {
     // UnionFindTree (UnionFind.scala)
   }
-  object EnRich {
-    implicit class AString(val self : String) extends AnyVal {
-      def splitToIntVector    = self.split(" ").map(_.toInt).toVector
-      def splitToDoubleVector = self.split(" ").map(_.toDouble).toVector
+  package io {
+    import java.io.{BufferedReader, PrintWriter, PrintStream, PushbackReader}
+    class MyConsole(val in: BufferedReader, val _out: PrintStream,
+                    val _err: PrintStream) {
+      // PrintWriter do not flush automatically
+      val out = new PrintWriter(_out,false)
+      // If argument is null, there are ambiguous which function will be called
+      def print(obj: Any)                  = out.print(if(obj == null) "null" else obj.toString)
+      def println()                        = out.println
+      def println(obj: Any)                = out.println(obj)
+      def printf(text: String, args: Any*) = out.printf(text.format(args : _*))
+      // NOTE: YOU MUST FLUSH BEFORE END OF MAIN
+      def flush()                          = out.flush
 
-      def splitToDoubleTuple2 = splitTuple2(() => splitToDoubleVector)
-      def splitToIntTuple2    = splitTuple2(() => splitToIntVector)
-      def splitToDoubleTuple3 = splitTuple3(() => splitToDoubleVector)
-      def splitToIntTuple3    = splitTuple3(() => splitToIntVector)
-
-      private def splitTuple2[X](splitTo: () => Vector[X]) = {
-        val ab = splitTo()
-        (ab(0),ab(1))
+      def nextInt()  : Int  = nextLong().toInt
+      def nextLong() : Long = {
+        if(!goNextValuable())
+          throw new NoSuchElementException("Reading long failed")
+        else{
+          val sgn = if(peek() == '-') -1l else 1
+          @tailrec
+          def readLong(next: Int, cur: Long) : Long =
+            if('0' <= next && next <= '9')
+              readLong(readWithoutCheckingPeeked(), cur*10 + next-'0')
+            else if(isSpaceOrControl(next))
+              sgn*cur
+            else
+              throw new NumberFormatException(s"readLong found strange byte $next")
+          readLong(read(),0)
+        }
       }
-      private def splitTuple3[X](splitTo: () => Vector[X]) = {
-        val abc = splitTo()
-        (abc(0),abc(1),abc(2))
+      def nextString() : String = {
+        if(!goNextValuable())
+          throw new NoSuchElementException("Reading String failed")
+        else {
+          val builder = new StringBuilder
+          @tailrec
+          def appendCode(next: Int) : String = {
+            if(isSpaceOrControl(next)){
+              builder.toString
+            }else{
+              builder.append(next.toChar)
+              appendCode(read())
+            }
+          }
+          appendCode(read())
+        }
+      }
+      // helpers
+      private[this] var peeked: Option[Int] = None
+      @inline private def read() =
+        peeked match {
+          case None    => in.read()
+          case Some(a) => { peeked = None; a }
+        }
+      @inline private def readWithoutCheckingPeeked() = in.read()
+      @inline private def peek() =
+        peeked match {
+          case None    => {
+            val res = in.read()
+            peeked  = Some(res)
+            res
+          }
+          case Some(a) => a
+        }
+      @inline private def isThereReadable() = peek() != -1
+      // XXX: this limits c is ASCII?
+      @inline private def isSpaceOrControl(c: Int) = c <= 32 || c == 127
+      @tailrec
+      final private def goNextNotSpaceNorControl() : Unit = {
+        if(isSpaceOrControl(peek())) {
+          read()
+          goNextNotSpaceNorControl()
+        }else {}
+      }
+      @inline private def goNextValuable() = {
+        goNextNotSpaceNorControl()
+        isThereReadable()
       }
     }
   }
 }
 
-import net.pushl.EnRich._
+import net.pushl.io.MyConsole
 object Main {
-  def main(args : Array[String]) : Unit = {
+  def main(args: Array[String]) : Unit = {
+    val console = new MyConsole(Console.in, Console.out, Console.err)
+    val solver  = new Solver(console)
+    solver.main()
+    console.flush()
   }
 }
+
+
+class Solver(val stdio: MyConsole){
+  import stdio._ // shadow Console.~
+  def main() : Unit = {
+    // write here.
+  }
+}
+
