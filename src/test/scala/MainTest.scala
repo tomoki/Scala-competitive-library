4d6c9ec00fc0dc3cc1956ae30bb927103975531f
Tomoki Imai <tomo832@gmail.com>
2015-07-20 00:57:49 +0900

    Add timeout (but not work)

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,7 +1,9 @@
 import org.scalatest.FlatSpec
+import org.scalatest.time._
+import org.scalatest.concurrent.Timeouts
 import java.nio.file.{Paths, Files}
 
-class MainSpec extends FlatSpec {
+class MainSpec extends FlatSpec with Timeouts {
   def execWithDummyStdio(main : (Array[String]) => Unit, input : String) : String = {
     val istream = new java.io.ByteArrayInputStream(input.getBytes)
     val ostream = new java.io.ByteArrayOutputStream
@@ -28,14 +30,20 @@ class MainSpec extends FlatSpec {
     fail("tests directory not found")
   }
   val input_files = lsFile(test_files_dir_str).filter((k) => getExtension(k.getName) == "in")
+  val timeout = 2000
   for(i <- input_files){
     val o = eraseExtension(i.getPath) + ".out"
     val exist_expected_file = Files.exists(Paths.get(o))
     i.getPath() should o in {
       assert(exist_expected_file)
       val dummy_in  = new String(Files.readAllBytes(Paths.get(i.getPath)))
-      val start     = System.currentTimeMillis();
-      val dummy_out = execWithDummyStdio(Main.main,dummy_in)
+      val start     = System.currentTimeMillis()
+      // not work because typically, program does not contain Threads.sleep or Object.wait
+      var dummy_out = ""
+      failAfter(Span(timeout, Millis)) {
+        dummy_out = execWithDummyStdio(Main.main,dummy_in)
+      }
+      // val dummy_out = execWithDummyStdio(Main.main,dummy_in)
       val end       = System.currentTimeMillis();
       val expected  = new String(Files.readAllBytes(Paths.get(o)))
       val interval  = end - start;
